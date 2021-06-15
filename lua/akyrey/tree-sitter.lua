ec3e23e5edbaa244ad21af3ced3057b8cc2f131f
Dario Vogogna <dario.vogogna@vargroup.it>
2021-06-15 22:41:11 +0200

    Moved config files to lua

--- lua/akyrey/tree-sitter.lua
+++ lua/akyrey/tree-sitter.lua
@@ -1,17 +1,35 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+local function init()
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+      'bash',
+      'css',
+      'dockerfile',
+      'html',
+      'javascript',
+      'jsdoc',
+      'json',
+      'lua',
+      'tsx',
+      'typescript',
     },
-  },
-  indent = {
-    enable = true
-  },
+    highlight = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+    indent = {
+      enable = true
+    },
+  }
+end
+
+return {
+  init = init
 }
--- plugin/navigation.vim
+++ plugin/navigation.vim
@@ -1,20 +1,3 @@
-" nnoremap <C-Left> :call AfPPAlternatePluthPluth()<CR>
-" nnoremap <C-Up> :call AfPPAlternate()<CR>
-" inoremap <C-Left> <esc>:call AfPPAlternatePluthPluth()<CR>
-" inoremap <C-Up> <esc>:call AfPPAlternate()<CR>
-" Go to next occurence in global quickfix list
-nnoremap <leader><C-k> :cnext<CR>zz
-" Go to previous occurence in global quickfix list
-nnoremap <leader><C-j> :cprev<CR>zz
-" Go to next occurence in local quickfix list
-nnoremap <leader>k :lnext<CR>zz
-" Go to previous occurence in local quickfix list
-nnoremap <leader>j :lprev<CR>zz
-" Open global quickfix list
-nnoremap <C-q> :call ToggleQFList(1)<CR>
-" Open local quickfix list
-nnoremap <leader>q :call ToggleQFList(0)<CR>
-
 let g:akyrey_qf_l = 0
 let g:akyrey_qf_g = 0
 
